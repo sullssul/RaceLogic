a0565d5af627d76fa1639409fd50fba7f9f5e3d7
Сулейман Сулейманов <sulejmanov.sulejma@net.ugatu.su>
2021-02-23 01:07:29 +0500

    new:Добавил темную тему, ландшафтный режим

--- app/src/main/java/com/example/racelogic/CLocation.java
+++ app/src/main/java/com/example/racelogic/CLocation.java
@@ -12,7 +12,6 @@ public class CLocation extends Location {
     }
 
     public CLocation(Location location, boolean bUseMetricUnits) {
-        // TODO Auto-generated constructor stub
         super(location);
         this.bUseMetricUnits = bUseMetricUnits;
     }
@@ -20,7 +19,7 @@ public class CLocation extends Location {
 
     public boolean getUseMetricUnits()
     {
-        return this.bUseMetricUnits;
+        return !this.bUseMetricUnits;
     }
 
     public void setUseMetricunits(boolean bUseMetricUntis)
@@ -30,9 +29,8 @@ public class CLocation extends Location {
 
     @Override
     public float distanceTo(Location dest) {
-        // TODO Auto-generated method stub
         float nDistance = super.distanceTo(dest);
-        if(!this.getUseMetricUnits())
+        if(this.getUseMetricUnits())
         {
             nDistance = nDistance * 3.28083989501312f;
         }
@@ -41,9 +39,8 @@ public class CLocation extends Location {
 
     @Override
     public float getAccuracy() {
-        // TODO Auto-generated method stub
         float nAccuracy = super.getAccuracy();
-        if(!this.getUseMetricUnits())
+        if(this.getUseMetricUnits())
         {
             nAccuracy = nAccuracy * 3.28083989501312f;
         }
@@ -52,9 +49,8 @@ public class CLocation extends Location {
 
     @Override
     public double getAltitude() {
-        // TODO Auto-generated method stub
         double nAltitude = super.getAltitude();
-        if(!this.getUseMetricUnits())
+        if(this.getUseMetricUnits())
         {
             nAltitude = nAltitude * 3.28083989501312d;
         }
@@ -63,9 +59,8 @@ public class CLocation extends Location {
 
     @Override
     public float getSpeed() {
-        // TODO Auto-generated method stub
         float nSpeed = super.getSpeed() * 3.6f;
-        if(!this.getUseMetricUnits())
+        if(this.getUseMetricUnits())
         {
             nSpeed = nSpeed * 2.2369362920544f/3.6f;
         }
--- app/src/main/java/com/example/racelogic/MainActivity.java
+++ app/src/main/java/com/example/racelogic/MainActivity.java
@@ -1,30 +1,27 @@
 package com.example.racelogic;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import androidx.appcompat.app.AppCompatDelegate;
 
 
 import android.annotation.SuppressLint;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.CheckBox;
 
 import android.widget.CompoundButton;
 import android.widget.TextView;
-
-
 
 
 public class MainActivity extends AppCompatActivity implements IBaseGpsListener {
@@ -33,52 +30,66 @@ public class MainActivity extends AppCompatActivity implements IBaseGpsListener 
     private TextView txtCurrentSpeed;
     private TextView txtMetricUnits;
     private CheckBox isHeadUpModeOn;
+    private CheckBox isDarkModeOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(isGeoDisabled()){
-
-
+        if (isGeoDisabled()) {
             CustomDialogFragment dialog = new CustomDialogFragment();
             dialog.show(getSupportFragmentManager(), "custom");
-
-                  //
-
-
 
         }
 
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         txtCurrentSpeed = findViewById(R.id.txtCurrentSpeed);
-        isHeadUpModeOn = findViewById(R.id.chkHeadUpModeOn);
         txtMetricUnits = findViewById(R.id.txtMetricUnits);
+        isHeadUpModeOn = findViewById(R.id.chkHeadUpModeOn);
+        isDarkModeOn = findViewById(R.id.chkDarkMode);
 
+        checkDarkMode();
 
-
-        if(checkLocationPermission()) {
+        if (checkLocationPermission()) {
             assert locationManager != null;
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
+    }
 
+    private void checkDarkMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            isDarkModeOn.setVisibility(View.VISIBLE);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode());
+
+            isDarkModeOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                    if (isChecked) {
+                        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else {
+                        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }
+
+                }
+            });
+        } else {
+            isDarkModeOn.setVisibility(View.GONE);
+        }
     }
 
 
-
-        public boolean isGeoDisabled() {
+    public boolean isGeoDisabled() {
         LocationManager mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            assert mLocationManager != null;
-            boolean mIsGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        assert mLocationManager != null;
+        boolean mIsGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         boolean mIsNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            return !mIsGPSEnabled && !mIsNetworkEnabled;
+        return !mIsGPSEnabled && !mIsNetworkEnabled;
     }
 
 
-    public boolean checkLocationPermission()
-    {
+    public boolean checkLocationPermission() {
         String permission = "android.permission.ACCESS_FINE_LOCATION";
         int res = this.checkCallingOrSelfPermission(permission);
         return (res == PackageManager.PERMISSION_GRANTED);
@@ -86,31 +97,27 @@ public class MainActivity extends AppCompatActivity implements IBaseGpsListener 
 
     @SuppressLint("SetTextI18n")
     private void updateSpeed(CLocation location) {
-        // TODO Auto-generated method stub
         int nCurrentSpeed = 0;
 
-        if(location != null)
-        {
+        if (location != null) {
             location.setUseMetricunits(this.useMetricUnits());
             nCurrentSpeed = (int) location.getSpeed();
         } else {
-            txtCurrentSpeed.setText("\t" +0 );
+            txtCurrentSpeed.setText("\t" + 0);
         }
 
 
         String strUnits = "m/h";
-        if(this.useMetricUnits())
-        {
+        if (this.useMetricUnits()) {
             strUnits = "km/h";
         }
 
         isHeadUpModeOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
+                if (isChecked) {
                     txtCurrentSpeed.setRotationX(180);
-                } else
-                {
+                } else {
                     txtCurrentSpeed.setRotationX(360);
                 }
             }
@@ -122,16 +129,13 @@ public class MainActivity extends AppCompatActivity implements IBaseGpsListener 
     }
 
     private boolean useMetricUnits() {
-        // TODO Auto-generated method stub
         CheckBox chkUseMetricUnits = this.findViewById(R.id.chkMetricUnits);
         return !chkUseMetricUnits.isChecked();
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        // TODO Auto-generated method stub
-        if(location != null)
-        {
+        if (location != null) {
             CLocation myLocation = new CLocation(location, this.useMetricUnits());
             this.updateSpeed(myLocation);
         }
@@ -139,25 +143,21 @@ public class MainActivity extends AppCompatActivity implements IBaseGpsListener 
 
     @Override
     public void onProviderDisabled(String provider) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void onGpsStatusChanged(int event) {
-        // TODO Auto-generated method stub
 
     }
 
